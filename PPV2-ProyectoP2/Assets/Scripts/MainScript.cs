44551c95175b33c853aca9046212db325b194a65
Sarai <sarai.sandoval@uteca.edu.mx>
2024-04-10 20:17:38 -0600

    Parcial-3 - Ventana de créditos y botón de Exit

--- PPV2-ProyectoP2/Assets/Scripts/MainScript.cs
+++ PPV2-ProyectoP2/Assets/Scripts/MainScript.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainScript : MonoBehaviour
 {
     public static MainScript instance;
     public string SelectedLesson = "dummy";
+
+    [Header("External GameObject Configuration")]
+    //esta es nuestra imagen principal que contiene la UI 
+    public GameObject creditos;
+
 
     private void Awake()
     {
@@ -31,6 +38,31 @@ public class MainScript : MonoBehaviour
         SceneManager.LoadScene("Lesson");
     }
 
+
+
+    public void EnableWindow()
+    {
+        //activeSelf es si está activado
+        if (creditos.activeSelf)
+        {
+            //desactiva el objeto si está activo
+            creditos.SetActive(false);
+        }
+        else
+        {
+            //activa el objeto si está desactivado
+            creditos.SetActive(true);
+            //MainScript.instance.SetSelectedLesson(LessonName);
+        }
+    }
+
+    public void QuitAplication()
+    {
+        if (Input.GetMouseButtonDown(0)) // Verifica si se hace clic con el botón izquierdo del mouse
+        {
+            Application.Quit(); // Cierra la aplicación
+        }
+    }
     void Start()
     {
         
